d192a1cddadc5e04ce3ce536e89b35f06b9514d2
Juliet Ndlamini <jndlamini3@gmail.com>
2023-10-08 01:17:09 -0700

    updated

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,13 +6,6 @@ Task 0: Add two floating-point numbers.
 
 def add(a: float, b: float) -> float:
     """
-    Adds two floating-point numbers and returns the result.
-
-    Parameters:
-        a (float): The first number to add.
-        b (float): The second number to add.
-
-    Returns:
-        float: The sum of a and b.
-    """
-    return a + b
+function add(a: number, b: number): number {
+  return a + b;
+}
